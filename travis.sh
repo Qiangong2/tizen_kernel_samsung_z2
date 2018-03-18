2f8faf6c102831f0be0beb247bd0c92574f7f013
ZhongQian TiaoGong <Qiangong2@users.noreply.github.com>
2018-03-18 17:15:30 +0000

    Update travis.sh
    debug time!

--- travis.sh
+++ travis.sh
@@ -3,5 +3,6 @@ export CROSS_COMPILE=./bluapprovedarmeabi4-8/arm-eabi-4.8/bin/arm-eabi-
 export ARCH=arm
 export KBUILD_BUILD_USER="Qiangong2"
 export KBUILD_BUILD_HOST="QG2ZHT1"
+"ls -al"
 make tizen_z2_defconfig
 make zImage
