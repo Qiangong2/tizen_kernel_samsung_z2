45f952165c38d661649be7f28ae5744cf1edffb5
ZhongQian TiaoGong <Qiangong2@users.noreply.github.com>
2018-03-18 19:37:52 +0000

    Update travis.sh
    Why do you have to be this way Travis?

--- travis.sh
+++ travis.sh
@@ -3,6 +3,5 @@ export CROSS_COMPILE=./bluapprovedarmeabi4-8/arm-eabi-4.8/bin/arm-eabi-
 export ARCH=arm
 export KBUILD_BUILD_USER="Qiangong2"
 export KBUILD_BUILD_HOST="QG2ZHT1"
-ls
-make tizen_z2_defconfig
+make /f ./Makefile tizen_z2_defconfig
 make zImage
