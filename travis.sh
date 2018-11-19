62d7c556802e006f6f5feccae8c22dab1278e590
ZhongQian TiaoGong <Qiangong2@users.noreply.github.com>
2018-11-19 11:11:45 -1000

    Update travis.sh
    Thanks ne0z!

--- travis.sh
+++ travis.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/Qiangong2/bluapprovedarmeabi4-8
-export CROSS_COMPILE=./bluapprovedarmeabi4-8/arm-eabi-4.8/bin/arm-eabi- 
+git clone https://github.com/ne0z/tizen_toolchain_kernel
+export CROSS_COMPILE=./tizen_toolchain_kernel/bin/arm-linux-gnueabi- 
 export ARCH=arm
 export KBUILD_BUILD_USER="Qiangong2"
 export KBUILD_BUILD_HOST="QG2ZHT1"
